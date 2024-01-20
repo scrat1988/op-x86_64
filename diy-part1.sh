36eb15fcc0c204f1186b0150410244faabd3bd09
scrat1988 <125626017+scrat1988@users.noreply.github.com>
2024-01-20 16:01:58 +0800

    Update diy-part1.sh
    change source

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,5 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
